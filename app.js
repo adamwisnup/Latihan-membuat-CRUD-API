13e53352ac126b6dce7d7b393eb39056bd4aac23
adamwisnup <wisnuadam21@gmail.com>
2024-04-03 23:32:39 +0700

    feat Add CORS support and Swagger documentation

--- app.js
+++ app.js
@@ -1,104 +1,23 @@
 require('dotenv').config();
 const express = require('express');
 const app = express();
+const cors = require('cors');
+const swaggerUi = require('swagger-ui-express');
+const fs = require('fs');
+const YAML = require('yaml');
 const port = process.env.PORT || 3000;
-const pg = require('pg');
 
-const pool = new pg.Pool({
-    user: process.env.DB_USER,
-    host: process.env.DB_HOST,
-    database: process.env.DB_NAME,
-    password: process.env.DB_PASSWORD,
-    port: process.env.DB_PORT,
-});
-
+const v1Router = require('./routes/v1/index');
 app.use(express.json());
+app.use(cors());
+app.use(express.urlencoded({ extended: true }));
 
-app.post('/api/students', async (req, res, next) => {
-    const { name, age, address, is_active } = req.body;
-    if (!name || !age || !address || is_active === undefined) {
-        return res.status(400).json({ message: 'Bad Request' });
-    }
-    try {
-        const newStudent = await pool.query('INSERT INTO students (name, age, address, is_active) VALUES ($1, $2, $3, $4) RETURNING *', [name, age, address, is_active]);
-        res.status(201).json({
-            message: 'Student created successfully',
-            student: newStudent.rows[0]
-        });
-    } catch (err) {
-        next(err);
-    }
-});
+//swagger
+const file = fs.readFileSync('./api-docs.yaml', 'utf8');
+const swaggerDocument = YAML.parse(file);
+app.use('/v1/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
-app.get('/api/students', async (req, res, next) => {
-    try {
-        let allStudents;
-
-        const keywords = req.query.keywords;
-
-        if (keywords) {
-            const searchQuery = 'SELECT * FROM students WHERE name LIKE $1 OR address LIKE $2';
-            const searchValues = [`%${keywords}%`, `%${keywords}%`];
-            allStudents = await pool.query(searchQuery, searchValues);
-        } else {
-            allStudents = await pool.query('SELECT * FROM students');
-        }
-        
-        res.status(200).json({
-            message: 'All Students successfully retrieved',
-            students: allStudents.rows
-        });
-    } catch (err) {
-        next(err);
-    }
-});
-
-
-app.get('/api/students/:id', async (req, res, next) => {
-    const { id } = req.params;
-    try {
-        const student = await pool.query('SELECT * FROM students WHERE id = $1', [id]);
-        if (student.rows.length === 0) {
-            res.status(404).json({
-                message: 'Student not found'
-            });
-        } else {
-            res.status(200).json({
-                message: 'Student retrieved successfully',
-                student: student.rows[0]
-            });
-        }
-    } catch (err) {
-        next(err);
-    }
-});
-
-app.put('/api/students/:id', async (req, res, next) => {
-    const { id } = req.params;
-    const { name, age, address, is_active } = req.body;
-    if (!name || !age || !address || is_active === undefined) {
-        return res.status(400).json({ message: 'Bad Request' });
-    }
-    try {
-        const updateStudent = await pool.query('UPDATE students SET name = $1, age = $2, address = $3, is_active = $4 WHERE id = $5 RETURNING *', [name, age, address, is_active, id]);
-        res.status(200).json({
-            message: 'Student updated successfully',
-            student: updateStudent.rows[0]
-        });
-    } catch (err) {
-        next(err);
-    }
-});
-
-app.delete('/api/students/:id', async (req, res, next) => {
-    const { id } = req.params;
-    try {
-        await pool.query('DELETE FROM students WHERE id = $1', [id]);
-        res.status(200).json({ message: 'Student deleted successfully' });
-    } catch (err) {
-        next(err);
-    }
-});
+app.use('/api/v1', v1Router);
 
 
 app.use((err, req, res, next) => {
